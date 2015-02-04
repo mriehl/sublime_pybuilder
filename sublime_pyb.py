4dc3360d9eedc7fa05d9694ec43f26c3c9aca89a
mriehl <max@riehl.io>
2015-02-04 13:06:57 +0100

    copyright update for 2015

--- sublime_pyb.py
+++ sublime_pyb.py
@@ -1,6 +1,6 @@
 #   The MIT License (MIT)
 
-#   Copyright (c) 2014 Maximilien Riehl <max@riehl.io>
+#   Copyright (c) 2014-2015 Maximilien Riehl <max@riehl.io>
 
 #   Permission is hereby granted, free of charge, to any person obtaining a copy
 #   of this software and associated documentation files (the "Software"), to deal
