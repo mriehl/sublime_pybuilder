53668e88ff6f032c3f45626cbed6832642e83fae
mriehl <max@riehl.io>
2015-02-04 14:31:45 +0100

    fixup github link

--- sublime_pyb.py
+++ sublime_pyb.py
@@ -255,7 +255,7 @@ def get_setting(name, mandatory=True):
         raise ExecutionError(dedent('''
                              Cannot find project setting {0}.
                              Please check the documentation for more information:
-                             \thttps://github.com/mriehl/sublime_pybuilder/README.md'''.format(name)))
+                             \thttps://github.com/mriehl/sublime_pybuilder/blob/master/README.md'''.format(name)))
     return setting
 
 
